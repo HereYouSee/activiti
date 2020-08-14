4c4f41adaee219afaa8d5e15a4f87cb00fb1257d
pmaster <pmaster@163.com>
2020-08-14 23:04:21 +0800

    add  test

--- masterv1/src/main/java/com/t1/TestMain.java
+++ masterv1/src/main/java/com/t1/TestMain.java
@@ -5,5 +5,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println(1111);
         System.out.println("linglang");
+        System.out.println(234);
     }
 }
